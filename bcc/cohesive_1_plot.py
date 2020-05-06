ba5009ad3a187990d166f15ea0f4b6b8ecf19749
suryabteja <suryabteja@gmail.com>
2020-05-06 17:56:34 +0530

    Add files via upload

---
+++ bcc/cohesive_1_plot.py
@@ -0,0 +1,28 @@
+﻿from numpy import *
+import matplotlib.pyplot as plt                 
+
+################################################################################
+# Load data.                                                                   #
+################################################################################
+
+a_2, pe_2 = loadtxt('Ni_BCC.dat', unpack=True)
+
+################################################################################
+# Plot.                                                                        #
+################################################################################
+
+# Start figure.
+fig = plt.figure()
+ax = fig.add_axes([0.15,0.15,0.80,0.80])
+
+# Plot.
+
+#ax.plot(a_2, pe_2, '--', c='g', lw=2, label='BCC Phase')
+
+ 
+
+# Add details and save figure.
+ax.set_xlabel(r'Lattice distance [Å]')
+ax.set_ylabel(r'Potential Energy per atom [eV/atom]')
+ax.legend(loc='best', frameon=False)
+fig.savefig("fig_free_energy_vs_temperature.png", dpi=300)
